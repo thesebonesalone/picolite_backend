dc410d7b11ef67dfe38385eaddab14b77c703d82
willemduiker <willem.duiker@smoothstack.com>
2022-09-06 12:03:37 -0500

    Prepping for Pull Merge

--- src/test/java/com/picolitebe/picolitebe/PicoLiteBeApplicationTests.java
+++ src/test/java/com/picolitebe/picolitebe/PicoLiteBeApplicationTests.java
@@ -1,12 +1,48 @@
 package com.picolitebe.picolitebe;
 
-import org.junit.jupiter.api.Test;
+
+import com.picolitebe.picolitebe.repository.ArticleRepository;
+import com.picolitebe.picolitebe.repository.CommentRepository;
+import com.picolitebe.picolitebe.service.ArticleService;
+import com.picolitebe.picolitebe.service.CommentService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-@SpringBootTest
-class PicoLiteBeApplicationTests {
+@Test
+class PicoLiteBeApplicationTests extends AbstractTestNGSpringContextTests {
 
-    @Test
+
+    @Mock
+    CommentRepository commentRepository;
+
+    @Mock
+    ArticleRepository articleRepository;
+
+    @InjectMocks
+    CommentService commentService;
+
+    @InjectMocks
+    ArticleService articleService;
+
+    @BeforeClass
+    public void init()
+    {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterClass
+    public void close() throws Exception
+    {
+        MockitoAnnotations.openMocks(this).close();
+    }
+
     void contextLoads() {
     }
 
