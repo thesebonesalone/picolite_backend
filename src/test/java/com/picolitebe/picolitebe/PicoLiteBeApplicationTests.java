069b8b98f5cb00d3d419c330c30ccf51b4ce559e
thesebonesalone <will.avery21@gmail.com>
2022-09-06 11:58:40 -0500

    Implementing Testing

--- src/test/java/com/picolitebe/picolitebe/PicoLiteBeApplicationTests.java
+++ src/test/java/com/picolitebe/picolitebe/PicoLiteBeApplicationTests.java
@@ -1,9 +1,11 @@
 package com.picolitebe.picolitebe;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 class PicoLiteBeApplicationTests {
 
     @Test
